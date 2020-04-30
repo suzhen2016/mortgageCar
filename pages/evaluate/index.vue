1affb398ec71cf27df27f721b0b009c886eff7dc
chenshengping <shengping@studytour.info>
2020-04-30 16:09:15 +0800

    first commit

---
+++ pages/evaluate/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>估价</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/sale/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>买车</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
